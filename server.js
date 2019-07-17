eb359e960ad6b9cf3895cccbe6cc532baf736486
Mahya <safavis@uci.edu>
2019-07-16 21:05:39 -0700

    making it work on heroku

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const {join}=require('path')
 app.use(express.static(join(__dirname,'./app/public')))
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
+const PORT = process.env.PORT || 3000
+
 
 const routes=require('./app/routing/index.js')
 routes(app)
-app.listen(3000)
+app.listen(PORT)
